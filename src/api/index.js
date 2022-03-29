fe688e11b8f0286ac6ff84c3b6b47520c77a0d09
hujiajia <1870641142@qq.com>
2022-03-29 23:14:45 +0800

    fix: feat/order

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@
 import requests from "./requests";
 
 export  const  reqCtogryList=()=>{
-  // return requests({url:'/product/getBaseCategoryList',method:'get'})
-  return requests({url:'http://127.0.0.1:4523/mock/792804/product/getBaseCategoryList',method:'get'})
+  return requests({url:'/product/getBaseCategoryList',method:'get'})
+  // return requests({url:'http://127.0.0.1:4523/mock/792804/product/getBaseCategoryList',method:'get'})
 
 }
