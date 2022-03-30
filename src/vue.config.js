564d56c3cf6f8da3ad8e110c315b2ddc0562aa4b
hjj1233 <51796354+hjj1233@users.noreply.github.com>
2022-03-31 00:20:47 +0800

    Feat/order (#3)
    * fix: merge dev
    
    * fix: merge dev

--- src/vue.config.js
+++ src/vue.config.js
@@ -3,11 +3,12 @@ module.exports={
   lintOnSave:false,
   //代理跨域
   devServer: {
-    // proxy: {
-    //   '/api': {
-    //     target: 'http://39.98.123.211',
-    //     // pathRewrite: {'^/api' : ''}
-    //   }
-    // }
+    proxy: {
+      '/api': {
+        // target: 'http://39.98.123.211',
+        target:'http://127.0.0.1:4523',
+        pathRewrite: {'^/api' : ''}
+      }
+    }
   }
 }
